ed0cb3b933963c0f58c5306a028517eb63287e3b
sanjay <sanjayskumar@xeoscript.com>
2022-05-21 19:49:53 +0530

    changed springfox

--- src/main/java/com/project/ams/web/EmployeeController.java
+++ src/main/java/com/project/ams/web/EmployeeController.java
@@ -4,7 +4,6 @@ import com.project.ams.data.models.Employee;
 import com.project.ams.data.payloads.request.EmployeeRequest;
 import com.project.ams.data.payloads.response.MessageResponse;
 import com.project.ams.service.EmployeeService;
-import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,13 +14,6 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/employee")
-//Line 19 to 24 is the newly added code for swagger documentation
-@ApiResponses(value = {
-        @io.swagger.annotations.ApiResponse(code = 400, message = "This is a bad request, please follow the API documentation for the proper request format"),
-        @io.swagger.annotations.ApiResponse(code = 401, message = "Due to security constraints, your access request cannot be authorized"),
-        @io.swagger.annotations.ApiResponse(code = 500, message = "The server is down. Please bear with us."),
-}
-)
 public class EmployeeController {
 
     @Autowired
