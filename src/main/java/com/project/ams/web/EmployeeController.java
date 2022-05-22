d85fd763dfff547bb1875ad53f80bf21996377d8
sanjay <sanjayskumar@xeoscript.com>
2022-05-22 12:41:31 +0530

    working with jsp

--- src/main/java/com/project/ams/web/EmployeeController.java
+++ src/main/java/com/project/ams/web/EmployeeController.java
@@ -7,6 +7,7 @@ import com.project.ams.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
